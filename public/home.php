76cc9a58686afad86930a539f80021a8d464b0ae
Wesley Carvalho <wesley0608romano@gmail.com>
2025-10-21 17:09:24 -0300

    Estilizando homepage e preparando conteúdos.

--- public/home.php
+++ public/home.php
@@ -5,14 +5,16 @@ defined('CONTROL') or die('Acesso negado!');
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="styles/home.css">
   <title>Showboard | Homepage</title>
 </head>
+
 <body>
 
   <header>
@@ -24,15 +26,24 @@ defined('CONTROL') or die('Acesso negado!');
   </header>
 
   <section class="hero">
-    <h2>Faça seu futuro e mostre sua evolução</h2>
-    <p>Gestão de projetos e portfólios na palma da mão</p>
-    <button onclick="document.getElementById('projetos-recentes').scrollIntoView({ behavior: 'smooth' });">
-  Ver Meus Projetos
-</button>
-
-
+    <h2>Mostre e divulgue seus melhores projetos!</h2>
+    <p>Gestão de projetos, crie, personalize e compartilhe porfólios interativos com facilidade! </p>
+    <a href="index.php?rota=dashboard" class="hero-btn">
+      Criar projeto
+    </a>
   </section>
-
+<section class="benefits">
+  <h2>Por que usar a Showboard?</h2>
+  <div class="card-container">
+    <div class="card-benefit">
+      <article class="card-article">
+        <div class="card-img">
+          <img src="#" alt="" class="cardi-img">
+        </div>
+      </article>
+    </div>
+  </div>
+</section>
   <section id="projetos-recentes" class="section">
     <h3>Projetos Recentes</h3>
     <div class="card">
@@ -46,4 +57,5 @@ defined('CONTROL') or die('Acesso negado!');
   </section>
 
 </body>
+
 </html>
